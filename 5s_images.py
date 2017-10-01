26c811b3f2a8b00bad3f80b0fdbb97201c98f573
Jake Chen <jakejchen@gmail.com>
2017-10-01 18:28:31 +0000

    bump

---
+++ 5s_images.py
@@ -0,0 +1,41 @@
+#
+# Takes a photo every 5 seconds
+# and pushes the photo to an S3 bucket
+#
+
+# Vars
+wait = 5
+s3_bucket = 'jakechenaws'
+
+# Libraries
+from time import sleep, gmtime, strftime
+from picamera import PiCamera
+import boto3
+
+# Initiate and warm-up camera
+camera = PiCamera()
+camera.resolution = (1024, 768)
+camera.start_preview()
+# Camera warm-up time
+sleep(2)
+
+# Main loop
+
+time_lapsed = 0
+while True:
+    camera.capture('image.jpg')
+ 
+    cur_time = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+    print('photo taken at {}'.format(cur_time))
+   
+    s3_key = 'projects/nvcam/image_'+str(time_lapsed)+'.jpg'
+    bucket = boto3.resource('s3').Bucket(s3_bucket)
+    bucket.upload_file('image.jpg', s3_key)
+
+    print('photo uploaded to {}'.format(s3_key))
+
+    sleep(wait)
+    time_lapsed += wait
+
+    if time_lapsed == 60:
+        time_lapsed = 0
--- test_image.py
+++ test_image.py
@@ -1,5 +1,6 @@
 from time import sleep
 from picamera import PiCamera
+import boto3
 
 camera = PiCamera()
 camera.resolution = (1024, 768)
@@ -7,3 +8,5 @@ camera.start_preview()
 # Camera warm-up time
 sleep(2)
 camera.capture('test.jpg')
+
+boto3.resource('s3').Bucket('jakechenaws').upload_file('test.jpg', 'projects/nvcam/test.jpg')
