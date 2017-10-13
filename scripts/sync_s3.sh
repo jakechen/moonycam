d3df06cbe009f90385cd23a045df65295d120b97
Jake Chen <jakejchen@gmail.com>
2017-10-12 22:37:16 -0500

    aws sync to cp

--- scripts/sync_s3.sh
+++ scripts/sync_s3.sh
@@ -1,1 +1,1 @@
-aws s3 sync ./images/latest/ s3://$s3_bucket/$s3_prefix --storage-class STANDARD_IA
+aws s3 cp ./images/latest/image_latest.jpg s3://$s3_bucket/$s3_prefix --storage-class STANDARD_IA
