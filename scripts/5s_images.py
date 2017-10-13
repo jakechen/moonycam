02227baa21cd4092bd5259eadaa2610d76ac8344
Jake Chen <jakejchen@gmail.com>
2017-10-12 23:13:25 -0500

    moved aws s3 cp back into python script

--- scripts/5s_images.py
+++ scripts/5s_images.py
@@ -1,5 +1,7 @@
 #
-# Takes a photo every 5 second
+# Takes a photo every ~5 second
+# depending on transfer time
+# 
 # Keeps 10 minutes worth locally, then loops
 #
 
@@ -11,6 +13,12 @@ keep_minutes = 10
 from time import sleep, gmtime, strftime
 from picamera import PiCamera
 import boto3
+import os
+
+# setup
+s3 = boto3.client('s3')
+s3_bucket = os.environ['s3_bucket']
+s3_prefix = os.environ['s3_prefix']
 
 # Initiate and warm-up camera
 camera = PiCamera()
@@ -31,7 +39,9 @@ while i<keep_count:
     cur_time = strftime("%Y-%m-%d %H:%M:%S", gmtime())
     print('photo taken at {}'.format(cur_time))
 
-    sleep(wait)
+    s3.upload_file('./images/latest/image_latest.jpg', s3_bucket, s3_prefix+'image_latest.jpg')
+
+    sleep(wait-2) # accoutn for transfer time
 
     i+=1
 
